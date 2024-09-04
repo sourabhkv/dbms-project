029aed89f7201afa0271d7281e154f017583604c
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2024-09-04 13:27:59 +0530

    .

--- Myapp/views.py
+++ Myapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from mysql.connector import connect, Error
 from django.http import HttpResponse
 
 from django.shortcuts import render  
--- cricket/settings.py
+++ cricket/settings.py
@@ -76,17 +76,12 @@ WSGI_APPLICATION = 'cricket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'dbms',  
-        'USER': 'root',  
-        'PASSWORD': 'qwerty@12345',  
-        'HOST': 'localhost',   # Or the IP Address of your MySQL server  
-        'PORT': '3306',        # The default port for MySQL is 3306  
-    }  
-}  
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
